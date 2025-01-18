f1a25e4c4f604e48a2e887218d414868e20c15a5
Pankaj Dhakad <pankaj.dhakad.india@gmail.com>
2025-01-18 09:29:33 +0530

    some file are add like admin and users

--- resources/views/users/layouts/signin.blade.php
+++ resources/views/users/layouts/signin.blade.php
@@ -115,7 +115,7 @@
                     </li>
                     <li>
                         <a href="index.html">
-                            Auction
+                            SIGN UP
                         </a>
                     </li>
                 </ul>
@@ -126,20 +126,29 @@
         </section>
         <!-- End Breadcrumb -->
         <!-- Login -->
-        <section id="login">
+        <section id="register">
             <div class="container">
                 <div class="account d-flex">
                     <div class="left-side">
                         <div class="header text-center">
-                            <h1>HI, THERE</h1>
-                            <p>You can log in to your Sbidu account here.</p>
+                            <h1>SIGN UP</h1>
+                            <p>We're happy you're here!</p>
                         </div>
                         <form action="#">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">
+                                    <i class="fa fa-user-o" aria-hidden="true"></i>
+                                </label>
+                                <input  class="form-control" type="text"
+                                name='email' placeholder="Enter Your Full Name">
+                            </div>
                             <div class="form-group">
                                 <label for="email">
                                     <i class="far fa-envelope"></i>
                                 </label>
-                                <input id="email" class="form-control" type="text" placeholder="Email Address">
+                                <input id="email" class="form-control" type="text"
+                                name='email' placeholder="Email Address">
                             </div>
                             <div class="form-group">
                                 <label for="password">
@@ -149,18 +158,21 @@
                                 <span class="show-password"><i class="fas fa-eye"></i></span>
                             </div>
                             <div class="form-group">
-                                <a href="#">Forgot Password?</a>
+                                <label for="password-comfirm">
+                                    <i class="fas fa-lock"></i>
+                                </label>
+                                <input id="password-comfirm" class="form-control" type="password" placeholder="Re-Password">
                             </div>
                             <div class="form-group mb-0 mt-4">
-                                <button type="submit">LOG IN</button>
+                                <button type="submit">REGISTER</button>
                             </div>
                         </form>
                     </div>
                     <div class="right-side">
                         <div class="sign-up">
-                            <h2>NEW HERE?</h2>
-                            <p>Sign up and create your Account</p>
-                            <a href="sing-up.html">SING UP</a>
+                            <h2>ALREADY HAVE AN ACCOUNT?</h2>
+                            <p>Log in and go to your Dashboard.</p>
+                            <a href="sing-in.html">LOGIN</a>
                         </div>
                     </div>
                 </div>
